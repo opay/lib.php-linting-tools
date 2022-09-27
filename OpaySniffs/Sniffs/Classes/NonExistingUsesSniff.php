78bdfc2ae0d0da3316bb824a7366fdb1dab0580a
Vitalij Vladimirov <vitalij.vladimirov@opay.lt>
2022-09-27 16:57:16 +0300

    LRVL-143: Replace Laravel code with plain php in rules

---
+++ OpaySniffs/Sniffs/Classes/NonExistingUsesSniff.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Opay\PhpLintingTools\Sniffs\Classes;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use SlevomatCodingStandard\Helpers\UseStatementHelper;
+
+class NonExistingUsesSniff implements Sniff
+{
+    public array $ignoreNamespaces = [];
+    public array $ignoreGlobalNamespaces = [
+        'OpenApi',
+        'JetBrains',
+    ];
+
+    public function register(): array
+    {
+        return [T_OPEN_TAG];
+    }
+
+    public function process(File $phpcsFile, $stackPtr): void
+    {
+        $statementsList = UseStatementHelper::getFileUseStatements($phpcsFile);
+        foreach ($statementsList as $statements) {
+            foreach ($statements as $statement) {
+                if ($this->isValidImport($statement->getFullyQualifiedTypeName())) {
+                    continue;
+                }
+
+                $phpcsFile->addError(
+                    'Namespace "%s" does not exist',
+                    $statement->getPointer(),
+                    'NonExistingNamespace',
+                    [$statement->getFullyQualifiedTypeName()]
+                );
+            }
+        }
+    }
+
+    private function isValidImport(string $namespace): bool
+    {
+        if (in_array($namespace, $this->ignoreNamespaces, true)) {
+            return true;
+        }
+
+        if (in_array(explode('\\', $namespace)[0], $this->ignoreGlobalNamespaces, true)) {
+            return true;
+        }
+
+        return class_exists($namespace) || interface_exists($namespace) || trait_exists($namespace);
+    }
+}
