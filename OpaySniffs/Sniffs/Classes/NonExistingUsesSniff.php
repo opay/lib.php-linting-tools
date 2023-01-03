ac9ed36016eb80f901cd3f08f8d0fd035e663d29
Vitalij Vladimirov <vitalij.vladimirov@opay.eu>
2023-01-03 18:15:15 +0200

    Add Klix to NonExistingUsesSniff ignore list

--- OpaySniffs/Sniffs/Classes/NonExistingUsesSniff.php
+++ OpaySniffs/Sniffs/Classes/NonExistingUsesSniff.php
@@ -14,6 +14,7 @@ class NonExistingUsesSniff implements Sniff
     public array $ignoreGlobalNamespaces = [
         'OpenApi',
         'JetBrains',
+        'Klix',
     ];
 
     public function register(): array
