94dc73e447a408ef3c0aa54c0b539f9dc43a49b0
Vitalij Vladimirov <vitalij.vladimirov@opay.lt>
2024-01-28 17:01:03 +0200

    CORE-83: Add ForbiddenCodeSniff rule

---
+++ OpaySniffs/Sniffs/Files/ForbiddenCodeSniff.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Opay\OpaySniffs\Sniffs\Files;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class ForbiddenCodeSniff implements Sniff
+{
+    public array $forbiddenCode = [
+        '<?=' => '<?php',
+    ];
+
+    private array $errorsOnLines = [];
+
+    public function register(): array
+    {
+        return [T_OPEN_TAG];
+    }
+
+    public function process(File $phpcsFile, $stackPtr): void
+    {
+        $file = file($phpcsFile->getFilename());
+
+        $tokens = $phpcsFile->getTokens();
+        foreach ($this->forbiddenCode as $forbiddenCode => $recommendedCode) {
+            if (is_int($forbiddenCode) === true) {
+                $forbiddenCode = $recommendedCode;
+                $recommendedCode = null;
+            }
+
+            foreach ($tokens as $key => $token) {
+                $line = $token['line'] - 1;
+                $lineContent = $file[$line];
+                if (str_contains($lineContent, $forbiddenCode) === false) {
+                    continue;
+                }
+
+                if ($token['column'] !== 1 || array_key_exists($line, $this->errorsOnLines)) {
+                    continue;
+                }
+
+                $errorMessage = $recommendedCode !== null
+                    ? sprintf('Forbidden code "%s" found. Use "%s" instead.', $forbiddenCode, $recommendedCode)
+                    : sprintf('Forbidden code "%s" found.', $forbiddenCode);
+
+                $phpcsFile->addError($errorMessage, $key, 'ForbiddenCodeFound');
+            }
+        }
+    }
+}
